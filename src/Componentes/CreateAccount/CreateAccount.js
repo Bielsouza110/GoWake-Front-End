39b6fe25a2ed3fbf7a940ed1c38c9ea866f288bf
gabrielsouza <benevidessouza2014@gmail.com>
2023-02-13 19:19:24 +0000

    details

--- src/Componentes/CreateAccount/CreateAccount.js
+++ src/Componentes/CreateAccount/CreateAccount.js
@@ -8,49 +8,66 @@ import {
 import {Button} from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import AppBar from "../home/AppBar";
+import Box from "@mui/material/Box";
+import {styled} from "@mui/material/styles";
+import {Container} from "@material-ui/core";
 
 function CreateAccount() {
     const navigate = useNavigate();
 
+    const DrawerHeader = styled('div')(({ theme }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+    }));
+
+
     return (
-        <div>
-            <AppBar/>
-            <div id="bg1">
-            <MDBContainer className="p-3 my-4 mg">
-                <MDBRow>
-                    <MDBCol id="esconde" col='10' md='6'>
-                        <img src="https://st2.depositphotos.com/1874273/9372/v/600/depositphotos_93724752-stock-illustration-athlete-on-board-glides-over.jpg"
-                             width="100%" height="100%" alt=""/>
-                    </MDBCol>
-                    <MDBCol col='4' md='6'>
-                        <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px', fontSize:'30px'
-                        }}>Join us</h5>
 
-                        <MDBInput wrapperClass='mb-4' placeholder="Email adress" id='formControlLg' type='email' size="lg"/>
-                        <MDBInput wrapperClass='mb-4' placeholder="Password" id='formControlLg' type='password' size="lg"/>
-                        <MDBInput wrapperClass='mb-4' placeholder="Confirm password" id='formControlLg' type='password' size="lg"/>
-                        <MDBInput wrapperClass='mb-4' placeholder="Insert invate code" id='formControlLg' type='email' size="lg"/>
+        <div className="sdd">
+            <Box sx ={{display:"flex"}}>
+                <AppBar/>
+                <Container>
+                    <DrawerHeader />
+                    <MDBContainer className="p-3 my-4 mg">
+                        <MDBRow>
+                            <MDBCol id="esconde" col='10' md='6'>
+                                <img src="https://st2.depositphotos.com/1874273/9372/v/600/depositphotos_93724752-stock-illustration-athlete-on-board-glides-over.jpg"
+                                     width="90%" height="90%" alt=""/>
+                            </MDBCol>
+                            <MDBCol col='4' md='6'>
+                                <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px', fontSize:'30px'
+                                }}>Join us</h5>
 
-                        <Button className="mb-3 w-100" size="lg">Sign Up</Button>
-                        <Button onClick={() => navigate(-1)} id="colorBTN" className="mb-4 w-100" size="lg">Cancel</Button>
+                                <MDBInput wrapperClass='mb-4' placeholder="Email adress" id='formControlLg' type='email' size="lg"/>
+                                <MDBInput wrapperClass='mb-4' placeholder="Password" id='formControlLg' type='password' size="lg"/>
+                                <MDBInput wrapperClass='mb-4' placeholder="Confirm password" id='formControlLg' type='password' size="lg"/>
+                                <MDBInput wrapperClass='mb-4' placeholder="Insert invate code" id='formControlLg' type='email' size="lg"/>
 
-                        <div className="divider d-flex align-items-center my-4"/>
+                                <Button className="mb-3 w-100" size="lg">Sign Up</Button>
+                                <Button onClick={() => navigate(-1)} id="colorBTN" className="mb-4 w-100" size="lg">Cancel</Button>
 
-                        <div className='d-flex flex-row justify-content-center'>
-                            <p id="fg" className="small text-muted me-1 fw-bold" >
-                            Already have an account? <a id="fgg" className="me-1 fw-bold" href="/login">
-                                    Sign in </a>
-                            </p>
-                        </div>
+                                <div className="divider d-flex align-items-center my-4"/>
 
-                        <div className='d-flex flex-row justify-content-center'>
-                            <a id="tu" href="#" className="small text-muted me-1">Terms of use.</a>
-                            <a id="pp" href="#" className="small text-muted">Privacy policy</a>
-                        </div>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-            </div>
+                                <div className='d-flex flex-row justify-content-center'>
+                                    <p id="fg" className="small text-muted me-1 fw-bold" >
+                                        Already have an account? <a id="fgg" className="me-1 fw-bold" href="/login">
+                                        Sign in </a>
+                                    </p>
+                                </div>
+
+                                <div className='d-flex flex-row justify-content-center'>
+                                    <a id="tu" href="#" className="small text-muted me-1">Terms of use.</a>
+                                    <a id="pp" href="#" className="small text-muted">Privacy policy</a>
+                                </div>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
+                </Container>
+            </Box>
         </div>
     );
 }
--- src/Componentes/Login/Login.js
+++ src/Componentes/Login/Login.js
@@ -5,48 +5,109 @@ import {
     MDBInput,
 } from 'mdb-react-ui-kit';
 import {Button} from "react-bootstrap";
-import { useNavigate } from 'react-router-dom';
+import {useHistory, useNavigate} from 'react-router-dom';
 import AppBar from "../home/AppBar";
 import Typography from "@mui/material/Typography";
+import {useEffect, useState} from "react";
+import {styled} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import DemoCarousel from "../home/DemoCarousel";
+import Cards from "../home/Cards";
+import Search from "../home/Search";
+import Subscribe from "../home/Subscribe";
+import Footer from "../home/Footer";
+import {Container} from "@material-ui/core";
 
 function Login() {
     const navigate = useNavigate();
+
+    const DrawerHeader = styled('div')(({ theme }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+    }));
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    /*const history = useHistory();*/
+    useEffect(() => {
+
+        if (localStorage.getItem("token")) {
+            navigate("/login/dashboard")
+        }
+    }, [])
+
+    async function login() {
+        console.warn(username, password)
+        let item = {username, password};
+        let result = await fetch("https://gowake.daletech.pt/api/auth/", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": 'application/json'
+            },
+            body: JSON.stringify(item)
+        });
+
+        result = await result.json();
+        localStorage.setItem("token", JSON.stringify(result))
+        navigate("/login/dashboard")
+    }
+
     return (
-        <div>
-            <AppBar/>
-            <div id="bg1">
-            <MDBContainer className="p-3 my-4">
-                <MDBRow>
-                    <MDBCol id="esconde" col='10' md='6'>
-                        <img src="https://st2.depositphotos.com/1874273/9372/v/600/depositphotos_93724752-stock-illustration-athlete-on-board-glides-over.jpg"
-                             width="100%" height="100%" alt=""/>
-                    </MDBCol>
-                    <MDBCol col='4' md='6'>
+            <div className="sdd">
+                <Box sx ={{display:"flex"}}>
+                    <AppBar/>
+                    <Container>
+                        <DrawerHeader />
+                        <MDBContainer className="p-3 my-4">
+                            <MDBRow>
+                                <MDBCol id="esconde" col='10' md='6'>
+                                    <img
+                                        src="https://st2.depositphotos.com/1874273/9372/v/600/depositphotos_93724752-stock-illustration-athlete-on-board-glides-over.jpg"
+                                        width="90%" height="90%" alt=""/>
+                                </MDBCol>
+                                <MDBCol col='4' md='6'>
 
-                        <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px', fontSize:'30px'
-                        }}>Sign into your account</h5>
+                                    <h5 className="fw-normal my-4 pb-3" style={{
+                                        letterSpacing: '1px', fontSize: '30px'
+                                    }}>Sign into your account</h5>
 
-                        <MDBInput wrapperClass='mb-4' placeholder="Email adress" id='formControlLg' type='email' size="lg"/>
-                        <MDBInput wrapperClass='mb-4' placeholder="Password" id='formControlLg' type='password' size="lg"/>
+                                    <MDBInput wrapperClass='mb-4' onChange={(e) => setUsername(e.target.value)}
+                                              placeholder="Email adress" id='formControlLg' type='text'
+                                              size="lg"/>
+                                    <MDBInput wrapperClass='mb-4' onChange={(e) => setPassword(e.target.value)}
+                                              placeholder="Password" id='formControlLg' type='password'
+                                              size="lg"/>
 
-                        <Button onClick={() => navigate("/login/dashboard")} className="mb-4 w-100" size="lg" >Sign in</Button>
+                                    {/* <Button onClick={login} className="mb-4 w-100" size="lg">Sign
+                                in</Button>*/}
 
-                        <div className="divider d-flex align-items-center my-4"/>
+                                    <Button onClick={() => navigate("/login/dashboard")} className="mb-4 w-100" size="lg">Sign
+                                        in</Button>
 
-                        <div className='d-flex flex-row justify-content-center'>
-                            <a id="fg" className="small text-muted me-1 fw-bold" href="#!">Forgot Password?</a>
-                            <a id="fg" className="small text-muted me-1 fw-bold" href="/createlogin">Create an Account</a>
-                        </div>
+                                    <div className="divider d-flex align-items-center my-4"/>
 
-                        <div className='d-flex flex-row justify-content-center'>
-                            <a id="tu" href="#" className="small text-muted me-1">Terms of use.</a>
-                            <a id="pp" href="#" className="small text-muted">Privacy policy</a>
-                        </div>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
+                                    <div className='d-flex flex-row justify-content-center'>
+                                        <a id="fg" className="small text-muted me-1 fw-bold" href="#!">Forgot Password?</a>
+                                        <a id="fg" className="small text-muted me-1 fw-bold" href="/createlogin">Create an
+                                            Account</a>
+                                    </div>
+
+                                    <div className='d-flex flex-row justify-content-center'>
+                                        <a id="tu" href="#" className="small text-muted me-1">Terms of use.</a>
+                                        <a id="pp" href="#" className="small text-muted">Privacy policy</a>
+                                    </div>
+                                </MDBCol>
+                            </MDBRow>
+                        </MDBContainer>
+                    </Container>
+                </Box>
             </div>
-        </div>
+
     );
 }
 
