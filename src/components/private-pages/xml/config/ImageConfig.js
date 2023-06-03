16e557de1c03de114195b7d70202b7fd259505ea
psingolas <sacramentojoel@gmail.com>
2023-06-03 13:23:51 +0100

    XML progress - v7 - xml icon

--- src/components/private-pages/xml/config/ImageConfig.js
+++ src/components/private-pages/xml/config/ImageConfig.js
@@ -1,11 +1,13 @@
 import fileDefault from '../../../../assets/file-blank-solid-240.png';
 import fileCss from '../../../../assets/file-css-solid-240.png';
 import filePdf from '../../../../assets/file-pdf-solid-240.png';
-import filepng from '../../../../assets/file-png-solid-240.png';
+import filePng from '../../../../assets/file-png-solid-240.png';
+import fileXml from '../../../../assets/xml.png';
 
 export const ImageConfig ={
     default: fileDefault,
     pdf: filePdf,
     css: fileCss,
-    png: filepng
+    png: filePng,
+    xml: fileXml
 }
