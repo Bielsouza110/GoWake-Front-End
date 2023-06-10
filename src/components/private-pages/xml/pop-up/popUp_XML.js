a61aae9e42df44caebbcfef9d3d6ecc9a31c7c85
psingolas <sacramentojoel@gmail.com>
2023-06-11 00:39:46 +0100

    XML progress - v13 - show Popup for test but for some reason I cant close it ahah

---
+++ src/components/private-pages/xml/pop-up/popUp_XML.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PopUp_XML = (props) => {
+    return (
+        <div>
+            <h1>Pop up </h1>
+            <p>This is the content of the new page.</p>
+        </div>
+    );
+};
+
+export default PopUp_XML;
