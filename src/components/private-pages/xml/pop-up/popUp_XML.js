ff2e07defdde40e13f36b0dc46b12daa8a5bc06b
psingolas <sacramentojoel@gmail.com>
2023-06-14 23:50:27 +0100

    XML progress - v14 - Can close the popup

--- src/components/private-pages/xml/pop-up/popUp_XML.js
+++ src/components/private-pages/xml/pop-up/popUp_XML.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
 
 const PopUp_XML = (props) => {
     return (
         <div>
             <h1>Pop up </h1>
             <p>This is the content of the new page.</p>
+
+
+
         </div>
     );
 };
