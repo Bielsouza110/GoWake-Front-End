27f34eab1b6a46585197b0fc613ac0edaecb3546
Biel Souza <benevidessouza2014@gmail.com>
2023-06-28 11:33:11 +0100

    Public private - in progress!

--- src/components/private-pages/xml/UploadXML.js
+++ src/components/private-pages/xml/UploadXML.js
@@ -3,40 +3,35 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Drawer from "../../../navs/Drawer";
 import DrawerHeader from "../../../navs/DrawerHeader";
-import {Container} from "@material-ui/core";
-import {MDBContainer} from "mdb-react-ui-kit";
+import { Container, Grid } from "@material-ui/core";
+import { MDBContainer } from "mdb-react-ui-kit";
 import './UploadXML.css';
 import DropFileInput from "./drop-file-input/DropFileInput";
 
 const UploadXML = () => {
     const onFileChange = (files) => {
         //console.log(files);
-    }
+    };
+
     return (
         <div className="sdd">
-            <Box sx={{display: "flex"}}>
-                <Drawer/>
-                <Container id="marginDrawerHeader">
-                    <DrawerHeader/>
-                    <MDBContainer className="p-1 my-2">
-                        <Typography paragraph className="my-3 pb-0">
-                            Page Upload XML
-                        </Typography>
+            <Box sx={{ display: "flex" }}>
+                <Drawer />
 
-                        <Container id="marginDrawerHeader">
-                            <div className="box">
-                                <h2 className="header">
-                                    Input XML file
-                                </h2>
-                                <DropFileInput onFileChange={(files) => onFileChange(files)}/>
-                            </div>
-                        </Container>
-
-                    </MDBContainer>
+                <Container id="marginDrawerHeader" className="top-margin">
+                    <DrawerHeader />
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <MDBContainer className="p-1 my-2">
+                                <Typography variant="h6" fontWeight="bold" className="my-3 pb-0" style={{ fontSize: '20px' }}>Input XML file</Typography>
+                                <DropFileInput onFileChange={onFileChange} />
+                            </MDBContainer>
+                        </Grid>
+                    </Grid>
                 </Container>
             </Box>
-
         </div>
     );
 };
+
 export default UploadXML;
